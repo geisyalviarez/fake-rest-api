38d3227ca38c607ef396cab75b026ea46693a89c
geisyalviarez <geisyalviarez@hamalsolutions.com>
2021-08-16 17:28:29 -0400

    start app in heroku

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 var express = require("express");
 var app = express();
 app.post("/authenticate", (req, res, next) => {
-  res.json({ token: "123456" });
+  res.json({
+    accesssToken: "xxxyyyzzz",
+    userId: "MarianaC",
+    userName: "Mariana Contreras",
+    userRol: "student",
+  });
 });
+
 app.listen(5001, () => {
   console.log("Server running on port 5001");
 });
