2196efd73e9a65c09f078266da389aed3c54a764
geisyalviarez <geisyalviarez@hamalsolutions.com>
2021-08-17 14:47:30 -0400

    coment

--- app.js
+++ app.js
@@ -13,44 +13,44 @@ app.post("/authenticate", (req, res, next) => {
   });
 });
 
-app.get(`/students/${studentId}`, (req, res, next) => {
-  res.json({
-    studentId: "1234",
-    studentName: "Mariana Contreras",
-    email: "gaaa@gmail.com",
-    phone: "4141234545",
-    enrollments: [
-      {
-        fileNumber: null,
-        registryNumber: 87654321,
-        career: "Contaduria Pública",
-        year: 1,
-        condition: "Nuevo ingreso",
-        amountPay: 10,
-        payments: [
-          {
-            currency: "bolívares",
-            method: "transferencia",
-            reference: "44444444",
-            date: 10 / 07 / 2021,
-            amount: 3,
-          },
-          {
-            currency: "dolares",
-            method: "cash",
-            reference: "123",
-            date: 10 / 07 / 2021,
-            amount: 7,
-          },
-        ],
-        course: [
-          { description: "math ", professor: "pedro" },
-          { description: "computation ", professor: "manuel" },
-        ],
-      },
-    ],
-  });
-});
+// app.get("/students/${studentId}", (req, res, next) => {
+//   res.json({
+//     studentId: "1234",
+//     studentName: "Mariana Contreras",
+//     email: "gaaa@gmail.com",
+//     phone: "4141234545",
+//     enrollments: [
+//       {
+//         fileNumber: null,
+//         registryNumber: 87654321,
+//         career: "Contaduria Pública",
+//         year: 1,
+//         condition: "Nuevo ingreso",
+//         amountPay: 10,
+//         payments: [
+//           {
+//             currency: "bolívares",
+//             method: "transferencia",
+//             reference: "44444444",
+//             date: 10 / 07 / 2021,
+//             amount: 3,
+//           },
+//           {
+//             currency: "dolares",
+//             method: "cash",
+//             reference: "123",
+//             date: 10 / 07 / 2021,
+//             amount: 7,
+//           },
+//         ],
+//         course: [
+//           { description: "math ", professor: "pedro" },
+//           { description: "computation ", professor: "manuel" },
+//         ],
+//       },
+//     ],
+//   });
+// });
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
